656c410e7dcb6fa70d625613152f22407ff52330
Dan Harms <dan.harms@dealer.com>
2013-06-05 13:38:26 -0400

    Controller action for page retrieval

--- thegame/grails-app/controllers/thegame/hudController.groovy
+++ thegame/grails-app/controllers/thegame/hudController.groovy
@@ -2,7 +2,17 @@ package thegame
 
 class hudController {
 
+    def springSecurityService
+    def pageService
+
     def index() {
         render("Webpage!")
+    }
+
+    def viewPage() {
+        int pageNumber = params.page ?: (springSecurityService.currentUser).currentStatus()
+        Map model = [displayPage: pageService.readPage(pageNumber)]
+
+        return model
     }
 }
