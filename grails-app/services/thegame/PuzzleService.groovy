5eaadfebac540a5f154ea78d385512e7350b1d6f
Dan Harms <dan.harms@dealer.com>
2013-06-06 11:03:48 -0400

    Bootstrapping some story pages Added answer submission

--- grails-app/services/thegame/PuzzleService.groovy
+++ grails-app/services/thegame/PuzzleService.groovy
@@ -13,27 +13,22 @@ class PuzzleService {
         )
     }
 
-    public boolean submitAnswer(String code, int pageNumber, String teamName) {
-        def team = springSecurityService.currentUser
+    public boolean submitAnswer(String code, int pageNumber) {
+        TeamDetails team = springSecurityService.currentUser
 
-        TeamDetails teamDetails = TeamDetails.findByTeamName(teamName)
-        String key = pageNumber.toString()
-        if(teamDetails.id != team?.id){
-            return false
-        }
-        if(![0,1].contains(pageNumber) && !teamDetails?.checkpointsCleared?.containsKey(Integer.toString(pageNumber - 1))){
+        if(!team?.checkpointsCleared?.containsKey(Integer.toString(pageNumber))){
             return false
         }
 
         Solution solution = Solution.findByPageNumber(pageNumber)
         boolean correct = solution.code.equalsIgnoreCase(code)
         if (correct) {
-            teamDetails.checkpointsCleared = teamDetails.checkpointsCleared ?: [:]
-            if(!teamDetails.checkpointsCleared.containsKey(key)){
-                teamDetails.checkpointsCleared.put(key, new Date())
-                teamDetails.save(flush:true)
+            String checkpoint = (pageNumber + 1).toString()
+            team.checkpointsCleared = team.checkpointsCleared ?: [:]
+            if(!team.checkpointsCleared.containsKey(checkpoint)){
+                team.checkpointsCleared.put(checkpoint, new Date())
+                team.save(flush:true)
             }
-
         }
         return correct
     }
